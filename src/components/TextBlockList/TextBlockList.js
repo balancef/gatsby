2f9bc4d0b144255ef4af7e1b287186ecbb58e5ab
Maximiliano Gonzalez <maximiliano.gonzalez@bitlogic.io>
2023-08-14 09:55:02 -0300

    custom textBlockList

--- src/components/TextBlockList/TextBlockList.js
+++ src/components/TextBlockList/TextBlockList.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { PortableText } from "@portabletext/react";
 import "./TextBlockList.scss";
-//import {FaEllipsisH} from 'react-icons/fa';
+import separador from "./images/image-separador.svg";
 
 const TextBlockList = ({ blocks }) => {
   const textBlocks = blocks?.map((block, id) => (
-    <div key={`textItem-${id}`} className="textItem">
-      <h5 className="mb-3 textItem__title">{block.title}</h5>
-      <div className="textItem__content">
-        <PortableText value={block?._rawContent} />
-        {/* {id < blocks.length - 1 && <FaEllipsisH className="textItem__icon" />} */}
+      <div key={`textItem-${id}`} className="textItem mb-3">
+          <h2 className="textItem__title">{block.title}</h2>
+          <div className="textItem__content">
+            <PortableText value={block?._rawContent} />
+          </div>
+          {id < blocks.length - 1 && <img src="separador" alt="separador"/>}
       </div>
-    </div>
   ));
   return (
     <div className="py-5 container">
