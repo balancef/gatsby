ca2c56ccc9150e77b2ac59481e348c14adc90e58
Maximiliano Gonzalez <maximiliano.gonzalez@bitlogic.io>
2023-08-14 12:51:54 -0300

    custom textBlockList

--- src/components/TextBlockList/TextBlockList.js
+++ src/components/TextBlockList/TextBlockList.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { PortableText } from "@portabletext/react";
 import "./TextBlockList.scss";
+import separadorImage from "../../images/image-separador.svg";
 
 
 const TextBlockList = ({ blocks }) => {
   const textBlocks = blocks?.map((block, id) => (
-      <div key={`textItem-${id}`} className="textItem mb-3">
-          <h2 className="textItem__title">{block.title}</h2>
-          <div className="textItem__content">
-            <PortableText value={block?._rawContent} />
-          </div>
-          {id < blocks.length - 1 && <img src="" />}
+      <div key={`textItem-${id}`} className="textItem">
+        <h2 className="textItem__title">{block.title}</h2>
+        {id < blocks.length - 1 && <img src={separadorImage} alt="separador" className="textItem__separador" />}
+        <div className="textItem__content">
+          <PortableText value={block?._rawContent} />
+        </div>
       </div>
   ));
   return (
