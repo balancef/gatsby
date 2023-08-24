f60cd9cffd61dd93326d7c96b6bbf172dc55ff59
Martin <martin.marino@bitlogic.io>
2023-08-24 12:05:16 -0300

    BAL-31: Help Page

---
+++ src/components/Help/data.js
@@ -0,0 +1,44 @@
+export const dataLanguage = {
+    es: {
+        seoTitle: "Ayuda",
+        seoDescription: "Pagina de ayuda",
+        pageTitle: "¿Cómo podemos ayudarte?",
+        placeholder: "Búsqueda por categoria o palabra clave",
+        button: "Ver más",
+        emptyState: "No se encontraron resultados",
+        component: {
+            title: "¿No ha habido suerte? Podemos ayudar.",
+            button: "Enviar Solicitud",
+            subTitle: "Nos pondremos en contacto con usted en 1-3 días laborables.",
+            link: "/es/contact",
+        }
+    },
+    de: {
+        seoTitle: "Hilfe",
+        seoDescription: "Hilfeseite",
+        pageTitle: "¿Wie können wir helfen?",
+        placeholder: "Suche nach Kategorie oder Schlüsselwort",
+        button: "Mehr sehen",
+        emptyState: "Keine Ergebnisse gefunden",
+        component: {
+            title: "Kein Glück? Wir können helfen!",
+            button: "Anfrage senden",
+            subTitle: "Wir melden uns bei Ihnen in 1-3 Werktagen.",
+            link: "/de/contact",
+        }
+    },
+    en: {
+        seoTitle: "Help",
+        seoDescription: "Help page",
+        pageTitle: "¿How can we help?",
+        placeholder: "Search by category or keyword",
+        button: "See more",
+        emptyState: "No results found",
+        component: {
+            title: "No luck? We can help.",
+            button: "Send Request",
+            subTitle: "We'll get back to you in 2-5 business days.",
+            link: "/contact",
+        }
+    },
+}
---
+++ src/pages/es/help.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Layout , Help} from "../../components"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../styles/global.scss";
+
+const HelpPage = () => {
+  return (
+    <Layout>
+      <Help/>
+    </Layout>
+  )
+}
+
+export default HelpPage
