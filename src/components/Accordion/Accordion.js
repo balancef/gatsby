4a0245b957c44f92786cb3aefe751e2081f800a8
Maximiliano Gonzalez <maximiliano.gonzalez@bitlogic.io>
2023-09-01 11:47:12 -0300

    BAL-45: Add condition in Accordion component

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -6,20 +6,21 @@ import { FaArrowRight } from "react-icons/fa"
 
 function AccordionComponent({ title, items }) {
   const accordionItems = items?.map((item, id) => (
-    <Accordion.Item eventKey={id} key={`item-${id}`}>
-      <Accordion.Header>
-        <p>{item.title}</p>
-        <icon>{<FaArrowRight className="icon" />}</icon>
-      </Accordion.Header>
-      <Accordion.Body>
-        <PortableText value={item._rawContent} />
-      </Accordion.Body>
-    </Accordion.Item>
+    item.title && item._rawContent.length > 0 ?
+      < Accordion.Item eventKey={id} key={`item-${id}`}>
+        <Accordion.Header>
+          <p>{item.title}</p>
+          <icon>{<FaArrowRight className="icon" />}</icon>
+        </Accordion.Header>
+        <Accordion.Body>
+          <PortableText value={item._rawContent} />
+        </Accordion.Body>
+      </Accordion.Item > : <></>
   ));
 
   return (
     <div className="AccordionComponent" >
-      <h5 className="mb-4 AccordionBlocks__title" id={title}>{title}</h5>
+      {title && <h5 className="mb-4 AccordionBlocks__title" id={title}>{title}</h5>}
       <Accordion defaultActiveKey="0" flush >
         {accordionItems}
       </Accordion>
