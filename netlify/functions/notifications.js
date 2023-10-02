2b5f07f768e50c1746d86fc0c7f75d2afee253e1
Martin <martin.marino@bitlogic.io>
2023-10-02 09:38:23 -0300

    test notifications

--- netlify/functions/notifications.js
+++ netlify/functions/notifications.js
@@ -2,6 +2,26 @@ const nodemailer = require('nodemailer');
 const fetch = require("node-fetch");
 const { schedule } = require("@netlify/functions");
 
+const sendMail = async (to, language) => {
+
+  language = language.toLowerCase()
+
+  const mailOptions = {
+    to: to,
+    cc: notification.result[0].CCemails,
+    subject: language === 'spanish' ? notification.result[0].subjectSpanish : language === 'german' ? notification.result[0].subjectGerman : notification.result[0].subject,
+    text: language === 'spanish' ? notification.result[0].templateSpanish : language === 'german' ? notification.result[0].templateGerman : notification.result[0].template,
+  };
+
+  try {
+    await client.sendMail(mailOptions);
+    console.log('Email enviado con éxito a: ' + to)
+  } catch (error) {
+    console.log(error);
+    console.log('Fallo el envio de mail para el profesional: ' + to);
+  }
+}
+
 const handler = async (event, context) => {
 
   const notificationResp = await fetch('https://dlep2zfh.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "notification"]{CCemails, subjectGerman, subjectSpanish, subject, daysToNotify, templateGerman, templateSpanish, template}')
@@ -19,26 +39,6 @@ const handler = async (event, context) => {
     }
   });
 
-  const sendMail = async (to, language) => {
-
-    language = language.toLowerCase()
-
-    const mailOptions = {
-      to: to,
-      cc: notification.result[0].CCemails,
-      subject: language === 'spanish' ? notification.result[0].subjectSpanish : language === 'german' ? notification.result[0].subjectGerman : notification.result[0].subject,
-      text: language === 'spanish' ? notification.result[0].templateSpanish : language === 'german' ? notification.result[0].templateGerman : notification.result[0].template,
-    };
-
-    try {
-      await client.sendMail(mailOptions);
-      console.log('Email enviado con éxito a: ' + to)
-    } catch (error) {
-      console.log(error);
-      console.log('Fallo el envio de mail para el profesional: ' + to);
-    }
-  }
-
   daysToNotify.forEach((day) => {
     const professionalsToNotify = professionals.result.filter(professional => {
       if (professional.validTo === null) return false
@@ -52,6 +52,7 @@ const handler = async (event, context) => {
     })
 
     console.log("Profesionales a notificar: " + professionalsToNotify.length + " para el día: " + day)
+    
     professionalsToNotify.forEach(async (professional) => {
       const language = professional.language !== null ? professional.language[0]?.language : 'English'
       await sendMail(professional.email, language)
